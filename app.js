28e8b5ffdc5a5b360cebb1a98825acf3720c472e
swethalakshmi22 <59462209+swethalakshmi22@users.noreply.github.com>
2020-03-25 18:35:37 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+const emailInput = document.getElementById('errorid');
+
+document.getElementById('press').addEventListener('click', e => {
+   e.preventDefault();
+   let emailVal = emailInput.value.trim();
+   if(emailVal === '') {
+     form.classList.add('error');
+     form.className.remove('success');
+   } else if(!validateEmail(emailVal)) {
+      form.className.remove('success');
+      form.className.add('error');
+   } else {
+      form.className.remove('error');
+      form.classList.add('success');
+   } 
+});
+
+// regex copied from StackOverflow //
+
+function validateEmail(emailInput) {
+   let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return valid.test(String(emailInput).toLowerCase());
+}
