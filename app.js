975806fd1397aa29e99ae00b30f53c1d1ea76589
swethalakshmi22 <59462209+swethalakshmi22@users.noreply.github.com>
2020-03-26 13:01:39 +0530

    updated

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-// let errorImg = document.getElementById('errorimg');
-// let emailInput = document.getElementById('enterid');
-// let errorMsg = document.querySelector('small');
+let errorImg = document.getElementById('errorimg');
+let emailInput = document.getElementById('enterid');
+let errorMsg = document.querySelector('small');
 
-// document.getElementById('press').addEventListener('click', e => {
-//    e.preventDefault();
-//    let emailVal = emailInput.value.trim();
-//    if(emailVal === '' || !validateEmail(emailVal)) {
-//       emailVal = '';
-//       emailInput.focus();
-//       errorImg.style.display = 'block';
-//       errorMsg.innerHTML = 'Please provide an valid email';
-//    } else {
-//       emailVal = '';
-//       emailInput.focus();
-//       errorImg.style.display = 'none';
-//       errorMsg.innerHTML = '';
-//    } 
-// });
+document.getElementById('press').addEventListener('click', e => {
+   e.preventDefault();
+   let emailVal = emailInput.value.trim();
+   if(emailVal === '' || !validateEmail(emailVal)) {
+      emailVal = '';
+      emailInput.focus();
+      errorImg.style.display = 'block';
+      errorMsg.innerHTML = 'Please provide an valid email';
+   } else {
+      emailVal = '';
+      emailInput.focus();
+      errorImg.style.display = 'none';
+      errorMsg.innerHTML = '';
+   } 
+});
 
-// // regex copied from StackOverflow //
+// regex copied from StackOverflow //
 
-// function validateEmail(emailInput) {
-//    let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//    return valid.test(String(emailInput).toLowerCase());
-// }
+function validateEmail(emailInput) {
+   let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   return valid.test(String(emailInput).toLowerCase());
+}
