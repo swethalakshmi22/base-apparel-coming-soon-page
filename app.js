9c2903605bbf1d3b6444df6ea82e27f548b0fba2
swethalakshmi22 <59462209+swethalakshmi22@users.noreply.github.com>
2020-03-26 12:59:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-let form = document.querySelector('form');
-let emailInput = document.getElementById('errorid');
+// let errorImg = document.getElementById('errorimg');
+// let emailInput = document.getElementById('enterid');
+// let errorMsg = document.querySelector('small');
 
-document.getElementById('press').addEventListener('click', e => {
-   e.preventDefault();
-   let emailVal = emailInput.value.trim();
-   if(emailVal === '') {
-     form.className.add('error');
-     form.className.remove('success');
-   } else if(!validateEmail(emailVal)) {
-      form.className.remove('success');
-      form.className.add('error');
-   } else {
-      form.className.remove('error');
-      form.className.add('success');
-   } 
-});
+// document.getElementById('press').addEventListener('click', e => {
+//    e.preventDefault();
+//    let emailVal = emailInput.value.trim();
+//    if(emailVal === '' || !validateEmail(emailVal)) {
+//       emailVal = '';
+//       emailInput.focus();
+//       errorImg.style.display = 'block';
+//       errorMsg.innerHTML = 'Please provide an valid email';
+//    } else {
+//       emailVal = '';
+//       emailInput.focus();
+//       errorImg.style.display = 'none';
+//       errorMsg.innerHTML = '';
+//    } 
+// });
 
-// regex copied from StackOverflow //
+// // regex copied from StackOverflow //
 
-function validateEmail(emailInput) {
-   let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return valid.test(String(emailInput).toLowerCase());
-}
+// function validateEmail(emailInput) {
+//    let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//    return valid.test(String(emailInput).toLowerCase());
+// }
